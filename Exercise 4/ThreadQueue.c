677ba3e9e1902385e19015b052014b57762df345
S Surya Narayanan <suryan0800@gmail.com>
2019-08-07 18:42:06 +0530

    Thread Queue is Created.

---
+++ Exercise 4/ThreadQueue.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+
+int queue[100];
+int front = -1, rear = -1;
+
+void *Producer()
+{
+    while(1)
+    {
+        
+        if(rear < 10)
+        {     
+            rear++;
+            queue[rear] = rand()/10000000;
+            sleep(0.5);
+        }
+        else{ break; }
+    }
+}
+
+void *Consumer()
+{
+    while(1)
+    {
+        if(front == 9)
+        {
+            break;
+        }
+        if(front < rear)
+        {
+            sleep(1);
+            front++;         
+            printf("Processed : %d \n",queue[front] );        
+        }
+        
+    }
+}
+
+void main()
+{
+    pthread_t produce,consume;
+    
+   
+
+    pthread_create(&produce,NULL,Producer,(void *)0);
+    pthread_create(&consume,NULL,Consumer,(void *)1);
+    
+    pthread_join(produce,NULL);
+    pthread_join(consume,NULL);
+
+
+}
